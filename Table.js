88343a656f4c9876101d3997e8e48110127c20c1
sureshbadikela <90884313+sureshbadikela@users.noreply.github.com>
2022-01-20 21:59:12 +0530

    Add files via upload

---
+++ Table.js
@@ -0,0 +1,172 @@
+
+import './Table.css'
+
+// const Table = () => {
+//     return (
+//         <div>
+        
+//         </div>
+//     )
+// }
+
+// export default Table
+
+// import faker from 'faker';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+    
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Avatar,
+    Grid,
+    Typography,
+    TablePagination,
+    TableFooter,
+    Button,
+ 
+ } from '@material-ui/core';
+ 
+
+const useStyles = makeStyles((theme) => ({
+    table: {
+      minWidth: 650,
+    },
+    tableContainer: {
+        borderRadius: 15,
+        margin: '10px 10px',
+        maxWidth: 950
+    },
+    tableHeaderCell: {
+        fontWeight: 'bold',
+        backgroundColor: theme.palette.primary.dark,
+        color: theme.palette.getContrastText(theme.palette.primary.dark)
+    },
+    avatar: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.getContrastText(theme.palette.primary.light)
+    },
+    name: {
+        fontWeight: 'bold',
+        color: theme.palette.secondary.dark
+    },
+    status: {
+        fontWeight: 'bold',
+        fontSize: '0.75rem',
+        color: 'white',
+        backgroundColor: 'grey',
+        borderRadius: 8,
+        padding: '3px 10px',
+        display: 'inline-block'
+    }
+  }));
+
+let USERS = [{name:"raj1234567890", email:"raj@gmail.com",phone:"123064567",joinDate:"1/1/1",jobTitle:"dev1234567890",company:"spsoft"},
+{email:"raj@gmail.com" ,name:"raj",phone:"123064567",joinDate:"1/1/1",jobTitle:"dev",company:"spsoft"},
+{phone:"123064567",email:"raj@gmail.com" ,name:"gaja",joinDate:"1/1/1",jobTitle:"dev",company:"spsoft"},
+{jobTitle:"dev",email:"raj@gmail.com" ,name:"raj",joinDate:"1/1/1",jobTitle:"dev",company:"spsoft"},
+{company:"spsoft",email:"raj@gmail.com" ,name:"raj",joinDate:"1/1/1",jobTitle:"dev"},
+{joinDate:"1/1/1",email:"raj@gmail.com" ,name:"raj",company:"spsoft",jobTitle:"dev"},
+
+
+], 
+// for(let i=0;i<14;i++) {
+//     USERS[i] = {
+//         name: faker.name.findName(),
+//         email: faker.internet.email(),
+//         phone: faker.phone.phoneNumber(),
+//         jobTitle: faker.name.jobTitle(),
+//         company: faker.company.companyName(),
+//         joinDate: faker.date.past().toLocaleDateString('en-US'),
+//         status: STATUSES[Math.floor(Math.random() * STATUSES.length)]
+//     }
+// }
+
+  MTable =()=> {
+  const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper} className={classes.tableContainer}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className={classes.tableHeaderCell}>User Info</TableCell>
+            <TableCell className={classes.tableHeaderCell}>Job Info</TableCell>
+            <TableCell className={classes.tableHeaderCell}>Joining Date</TableCell>
+             <TableCell className={classes.tableHeaderCell}>Action</TableCell> 
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {USERS.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
+            <TableRow key={row.name}>
+              <TableCell>
+                  <Grid container>
+                      <Grid item lg={2}>
+                          <Avatar alt={row.name} src='.' className={classes.avatar}/>
+                      </Grid>
+                      <Grid item lg={10}>
+                          <Typography className={classes.name}>{row.name}</Typography>
+                          <Typography color="textSecondary" variant="body2">{row.email}</Typography>
+                          <Typography color="textSecondary" variant="body2">{row.phone}</Typography>
+                      </Grid>
+                  </Grid>
+                </TableCell>
+              <TableCell>
+                  <Typography color="primary" variant="subtitle2">{row.jobTitle}</Typography>
+                  <Typography color="textSecondary" variant="body2">{row.company}</Typography>
+                </TableCell>
+              <TableCell>{row.joinDate}</TableCell>
+              <TableCell>
+              <Button variant="outlined">edit</Button>
+             
+          
+        
+              </TableCell>
+              {/* <TableCell>
+                  <Typography 
+                    className={classes.status}
+                    style={{
+                        backgroundColor: 
+                        ((row.status === 'Active' && 'green') ||
+                        (row.status === 'Pending' && 'blue') ||
+                        (row.status === 'Blocked' && 'orange'))
+                    }}
+                  >{row.status}</Typography>
+                </TableCell> */}
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+        <TablePagination
+            rowsPerPageOptions={[5, 10, 15]}
+            component="div"
+            count={USERS.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default MTable;
+
